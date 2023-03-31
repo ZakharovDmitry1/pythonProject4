b9eb489f6628d2d50d0d384f25bf5b3f6286e26a
ZakharovDmitry1 <vbnzvbnz1234567890@gmail.com>
2023-03-31 22:07:29 +0300

    По каютам!(WEB. Шаблоны. flask-wtf)

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 temmplate = env.get_template("index.html")
 
-
+items = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни',
+                  'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
 @app.route('/')
-@app.route('/list_prof/<p>')
+@app.route('/distribution/')
 def sample_file_upload():
-    return render_template('index.html')
+    return render_template('index.html', items=items)
 
 
 if __name__ == '__main__':
