7232bb57fe54bf6d84e66c146a9b0e04faa42ef0
ZakharovDmitry1 <vbnzvbnz1234567890@gmail.com>
2023-03-31 17:01:40 +0300

    Двойная защита(WEB. Шаблоны. flask-wtf)

--- main.py
+++ main.py
@@ -8,16 +8,11 @@ file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 temmplate = env.get_template("index.html")
 
-items = "инженер-исследователь, пилот, строитель, экзобиолог, врач," \
-        " инженер по терраформированию, климатолог, специалист по радиационной защите," \
-        " астрогеолог, гляциолог, инженер жизнеобеспечения, метеоролог, оператор марсохода," \
-        " киберинженер, штурман, пилот дронов".split(', ')
-
 
 @app.route('/')
 @app.route('/list_prof/<p>')
-def sample_file_upload(p=''):
-    return render_template('index.html', items=items, list=p)
+def sample_file_upload():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
